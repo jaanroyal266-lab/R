442374060cee80c8e3e23c7f2cb6e7f0ad46ce34
teamsuraj2 <surajsingh80092002@gmail.com>
2025-11-02 22:21:32 +0530

    Update config.go

--- config/config.go
+++ config/config.go
@@ -44,7 +44,7 @@ var (
 
 	// Optional Vars
 	ApiKEY         = getString("FALLEN_API_KEY")
-	ApiURL         = getString("FALLEN_API_URL", "https://tgmusic.fallenapi.fun")
+	ApiURL         = getString("FALLEN_API_URL", "https://panel.thequickearn.xyz/")
 	OwnerID        = getInt64("OWNER_ID")
 	LoggerID       = getInt64("LOGGER_ID")
 	DurationLimit  = int(getInt64("DURATION_LIMIT", 4200)) // in seconds
