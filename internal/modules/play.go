a2404526487def0a7f022c25aead726490e6b524
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-11-06 06:13:12 +0530

    Add retry mechanism for track playback on flood wait
    Implement retry logic for playing track on flood wait error.

--- internal/modules/play.go
+++ internal/modules/play.go
@@ -214,8 +214,15 @@ func handlePlay(m *telegram.NewMessage, force, cplay bool) error {
 			logger.InfoF("Downloaded track to %s", filePath)
 		}
 		if err := r.Play(track, filePath, force && i == 0); err != nil {
-			utils.EOR(replyMsg, "❌ Failed to play\nError: "+err.Error())
-			return err
+			wait := telegram.GetFloodWait(err)
+			if wait > 0 {
+				time.Sleep(time.Duration(wait) * time.Second)
+				err = r.Play(track, filePath, force && i == 0)
+			}
+			if err != nil {
+				utils.EOR(replyMsg, "❌ Failed to play\nError: "+err.Error())
+				return err
+			}
 		}
 		sendPlayLogs(m, track, (isActive && !force) || i > 0)
 	}
